d4f9e862b6de62ac7c3dda8bda7dbc0bccc8e557
MAUMOREIRA <mauriciomoreirajr2006@gmail.com>
2023-05-09 12:17:32 -0300

    blz

--- altera_usuario.php
+++ altera_usuario.php
@@ -1,6 +1,6 @@
 <?php
-    include('conexão.php');
-    $id_usuario = $_GET('id_usuario');
+    include('conexao.php');
+    $id_usuario = $_GET['id_usuario'];
     $sql = "SELECT * FROM usuario where id_usuario=$id_usuario";
     $result = mysqli_query($con,$sql);
     $row = mysqli_fetch_array($result)
@@ -14,29 +14,31 @@
     <title>Document</title>
 </head>
 <body>
+    <!--http://localhost/PRW/Banco%20de%20dados/Aulas/altera_usuario?id_usuario=1 -->
     <h1>Cadastro de Usuarios - IFSP</h1>
     <form action="altera_usuario_exe.php" method="POST" >
         <div>
             <label for="nome">Nome</label>
-            <input type="text" name="nome" id="nome">
-            value="<php echo $row 'nome_usuario' ?>">
+            <input type="text" name="nome" id="nome"
+            value="<?php echo $row['nome_usuario'] ?>">
         </div>
         <div>
             <label for="email">E-mail</label>
-            <input type="email" name="email" id="email">
-            value="<php echo $row 'email_usuario' ?>">
+            <input type="email" name="email" id="email"
+            value="<?php echo $row['email_usuario'] ?>">
+            
         </div>
         <div>
             <label for="fone">Telefone</label>
             <input type="tel" name="fone" id="fone" 
             placeholder="Formato (18) 9999-8888"
-            pattern="\([0-9]{2}\)([9]{1})?[0-9]{4-5}-[0-9]{4}">
-            value="<php echo $row 'fone_usuario' ?>">
+            pattern="\([0-9]{2}\)([9]{1})?[0-9]{4-5}-[0-9]{4}"
+            value="<?php echo $row['fone_usuario'] ?>">
         </div>
         <div>
             <label for="senha">Senha</label>
-            <input type="password" name="senha" id="senha">
-            value="<php echo $row 'senha' ?>">
+            <input type="password" name="senha" id="senha"
+            value="<?php echo $row['senha'] ?>">
         </div>
         <div>
             <input type="submit" value="Salvar">
