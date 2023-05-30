1f7e5215078008b9d88bb2a38b801c40482deac4
MAU MOREIRA <124677551+MAUMOREIRA@users.noreply.github.com>
2023-05-30 12:20:37 -0300

    Update cadastro_usuario.php

--- cadastro_usuario.php
+++ cadastro_usuario.php
@@ -1,25 +1,35 @@
 <?php
+    include("conexao.php");
+    // Upload foto
+    if(file_exists($_FILES['foto']['tmp_name'])){
+    $pasta_destino = 'fotos/';
+    $extensao = strtolower(substr($_FILES['foto']['name'],-4));
+    $nome_foto = $pasta_destino . date("Ymd-His") . $extensao;
+    move_uploaded_file($_FILES['foto']['tmp_name'], $nome_foto);
+    }
+    // fim upload
 
-include("conexao.php");
-$nome = $_POST['nome']; 
-$email = $_POST['email'];
-$fone = $_POST['fone'];
-$senha = $_POST['senha'];
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $fone = $_POST['fone'];
+    $senha = $_POST['senha'];
 
-echo "<h1> Dados do usuário</h1>";
-echo "Nome: $nome <br>";
-echo "Email: $email <br>";
-echo "telefone: $fone <br>";
-echo "Senha: $senha <br>";
+    echo "<h1>Dados do usuário</h1>";
+    echo "Nome: $nome <br>";
+    echo "E-mail: $email <br>";
+    echo "Telefone: $fone <br>";
+    echo "Senha: $senha <br>";
 
-$sql = "INSERT INTO usuario (nome_usuario,email_usuario, fone_usuario,senha)";
-$sql .= " VALUES ('".$nome. "','".$email."','". $fone. "','".$senha."')";
+    $sql = "INSERT INTO usuario (nome_usuario, email_usuario, fone_usuario, senha, foto)";
 
-echo $sql. "<br>";
-$result = mysqli_query($con,$sql);
-if($result)
-echo "Dados cadastrados com sucesso!";
+    $sql .= " VALUES ('".$nome."','".$email."','".$fone."','".$senha."','".$nome_foto."')";
 
-else
-echo "Erro ao tentar cadastrar!";
+    echo $sql."<br>";
+    $result = mysqli_query($con, $sql);
+
+    if ($result)
+        echo "Dados cadastrados com sucesso!";
+    else
+        echo "Erro ao tentar cadastrar!";
+        
 ?>
